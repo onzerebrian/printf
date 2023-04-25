90ccc11722535c7e40f03818293d7531c1c87ce6
Engefu <57341697+Engefu@users.noreply.github.com>
2023-04-25 15:00:29 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,13 +1,17 @@
-#ifndef PRINT_H
-#define PRINT_H
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
 
 
 int _printf(const char *format, ...);
-int p_char(va_list al);
-int char_print(char c);
-int p_string(va_list al);
-int p_percent();
-int p_number(va_list al);
+int output_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
+
+/**int p_char(va_list al);
+* int char_print(char c);
+* int p_string(va_list al);
+* int p_percent();
+*/ int p_number(va_list al);
 #endif
